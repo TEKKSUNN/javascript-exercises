1291d4dfcc7f310b2fa23ccbf8e735a5b357bfe9
turwokts <48319429+TEKKSUNN@users.noreply.github.com>
2024-07-06 22:07:01 +0800

    Add repeatString function & return correctly

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(message, num) {
+    if (num < 0)
+    {
+        return 'ERROR';
+    }
+    arr = [];
+    for (let i = 0; i < num; i++)
+    {
+        arr.push(message);
+    }
+    return arr.join("");
 };
 
 // Do not edit below this line
