f1dc48847ba0d4f110b4ae7ab148febc85a08b9b
turwokts <48319429+TEKKSUNN@users.noreply.github.com>
2024-07-07 11:50:51 +0800

    Add sumAll func code & pass

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
-
+const sumAll = function(firstNumber, secondNumber) {
+    let sum = 0;
+    if (typeof firstNumber != "number" || typeof secondNumber != "number" ||
+    firstNumber < 0 || secondNumber < 0)
+    {
+        return 'ERROR';
+    }
+    if (firstNumber < secondNumber)
+    {
+        for (let i = firstNumber; i <= secondNumber; i++)
+        {
+            sum += i;
+        }
+        return sum;
+    }
+    if (firstNumber === secondNumber)
+    {
+        return firstNumber + secondNumber;
+    }
+    for (let i = firstNumber; i >= secondNumber; i--)
+    {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
