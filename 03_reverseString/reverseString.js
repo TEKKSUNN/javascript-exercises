221f776cebc1a7190d4bd10fd6bf1df6107c9097
turwokts <48319429+TEKKSUNN@users.noreply.github.com>
2024-07-07 11:14:28 +0800

    Add reverseString code & pass test

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(strng) {
+    newString = [];
+    for (let i = strng.length - 1, j = 0; i >= 0; i--, j++)
+    {
+        newString[j] = strng[i];
+    }
+    return newString.join("");
 };
 
 // Do not edit below this line
