e50a9e356ee2b0b9a6f3e9cdd4efa4d018651d18
turwokts <48319429+TEKKSUNN@users.noreply.github.com>
2024-07-13 19:01:15 +0800

    Add calc funcs & pass all tests

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(numsArray) {
+	return numsArray.reduce((total, currentNum) => total += currentNum, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(numsArray) {
+  return numsArray.reduce((total, currentNum) => total *= currentNum, 1);
 };
 
-const power = function() {
-	
+const power = function(num, toPower) {
+	// return num ** toPower; - Easy way, but I ain't this weak
+  return ((new Array(toPower)).fill(num)).reduce((total, currentNum) => total *= currentNum, 1); // Haha, weaklings, i can do this
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let finalNum = 1;
+  for (let i = 1; i <= num; i++)
+  {
+    finalNum *= i;
+  }
+  return finalNum;
 };
 
 // Do not edit below this line
