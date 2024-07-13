ae4c9f9d3bfb13294439a638cedffabbf7a83bae
turwokts <48319429+TEKKSUNN@users.noreply.github.com>
2024-07-13 20:35:59 +0800

    Add fibonacci function code

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,25 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+  if (number === 0 || +number === 0)
+  {
+    return 0;
+  }
+  if (number < 0)
+  {
+    return 'OOPS';
+  }
+  if (Number.isInteger(+number))
+  {
+    number = +number;
+  }
+  else {
+    return 'OOPS';
+  }
+  let fibonacciList = [1, 1];
+  for (let i = 2; i < number; i++)
+  {
+    fibonacciList[i] = fibonacciList[i-1] + fibonacciList[i-2];
+  }
+  return fibonacciList[number - 1];
 };
 
 // Do not edit below this line
