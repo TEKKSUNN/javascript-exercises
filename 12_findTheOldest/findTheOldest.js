37578abca1d1d7b8494f98070b9c6e7d7440e7a8
turwokts <48319429+TEKKSUNN@users.noreply.github.com>
2024-07-13 22:12:21 +0800

    Add 'findTheOldest' func code

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,10 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arrayKwarg) {
+  return arrayKwarg
+              .sort((personLast, personNext) => {
+                personLastDeath = personLast.yearOfDeath === undefined ? (new Date()).getFullYear() : personLast.yearOfDeath;
+                personNextDeath = personNext.yearOfDeath === undefined ? (new Date()).getFullYear() : personNext.yearOfDeath;
+                return (personLastDeath - personLast.yearOfBirth) - (personNextDeath - personNext.yearOfBirth);
+              })[arrayKwarg.length - 1];
 };
 
 // Do not edit below this line
