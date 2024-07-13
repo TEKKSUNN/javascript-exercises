bd063c34c7effdd1a7e4cd88ff78ed375055e2e4
turwokts <48319429+TEKKSUNN@users.noreply.github.com>
2024-07-13 19:57:50 +0800

    Add palindrome checker func

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
-
+const palindromes = function (stringValue) {
+  const ALLOWED_CHARACTERS = 'abcdefghijklmnopqrstuvwxyz0123456789';
+  let newString = stringValue.split(' ');
+  newString = newString.join('');
+  newString = newString.toLowerCase();
+  newString = (newString.split('')).filter((item) => ALLOWED_CHARACTERS.includes(item));
+  newString = newString.join('');
+  reversedNewString = [];
+  for (let i = newString.length - 1, j = 0; i >= 0; i--, j++)
+  {
+    reversedNewString[j] = newString[i];
+  }
+  reversedNewString = reversedNewString.join('');
+  return newString == reversedNewString;
 };
 
 // Do not edit below this line
